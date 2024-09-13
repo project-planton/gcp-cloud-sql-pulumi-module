a7c8e824ab2b7b527843b7229fe2f61b2b52d623
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-09-14 03:25:07 +0530

    converted to pulumi git repo

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # gcp-cloud-sql-pulumi-module
 
 Pulumi Module to Deploy "Relational Database on Google CloudSQL" w/ Planton Cloud
+
+```shell
+pulumi stack select --stack <pulumi-org-name>/<pulumi-stack-name>
+```
+
+```shell
+export STACK_INPUT_FILE_PATH=<path-to-stack-input-yaml-file>
+pulumi up
+```
--- pkg/outputs/outputs.go
+++ pkg/outputs/outputs.go
@@ -1,11 +1,5 @@
 package outputs
 
-import (
-	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/gcp/gcpcloudsql"
-	"github.com/pulumi/pulumi/sdk/v3/go/auto"
+const (
+	OutputKey = "value"
 )
-
-func PulumiOutputsToStackOutputsConverter(stackOutput auto.OutputMap,
-	input *gcpcloudsql.GcpCloudSqlStackInput) *gcpcloudsql.GcpCloudSqlStackOutputs {
-	return &gcpcloudsql.GcpCloudSqlStackOutputs{}
-}
