991c86a272d62215a2caacb1996e90c023846472
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-10-02 11:04:53 +0530

    updated project-planton deps that has apis removed from package name

--- pkg/resources.go
+++ pkg/resources.go
@@ -1,7 +1,7 @@
 package pkg
 
 import (
-	gcpcloudsqlv1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/apis/provider/gcp/gcpcloudsql/v1"
+	gcpcloudsqlv1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/provider/gcp/gcpcloudsql/v1"
 	"github.com/pkg/errors"
 	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/provider/gcp/pulumigoogleprovider"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
